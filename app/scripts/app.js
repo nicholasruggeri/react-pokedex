d56b9e1e815846a1d23dd3e75def76dd5b48ab15
nicholasruggeri <ruggerinicholas@gmail.com>
2015-11-17 09:57:40 +0100

    add PokemonImage module

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,24 +1,33 @@
 
 var React = window.React = require('react'),
-    ReactDOM = require("react-dom"),
-    Timer = require("./ui/Timer"),
-    mountNode = document.getElementById("app");
+    ReactDOM = require("react-dom");
+
+var mountNode = document.getElementById("app");
+
+
+var PokemonImage = React.createClass({
+  render: function() {
+    return (
+      <img src={"http://pokeapi.co/media/img/" + this.props.id + ".png"} />
+    );
+  }
+});
 
 
 var PokemonType = React.createClass({
   render: function() {
-
     return (
-      <span>{this.props.type.name} </span>
+      <span className={this.props.type.name}>{this.props.type.name} </span>
     );
   }
-})
+});
 
 var Pokemon = React.createClass({
 
   getInitialState: function() {
     return {
       name: '',
+      id: '',
       types: []
     }
   },
@@ -27,10 +36,10 @@ var Pokemon = React.createClass({
 
     $.get('http://pokeapi.co/api/v1/pokemon/' + this.props.id + '/', function(result) {
       if (this.isMounted()) {
-        console.log(result)
         this.setState({
           name: result.name,
-          types: result.types
+          id: result.pkdx_id,
+          types: result.types,
         });
       }
     }.bind(this));
@@ -42,7 +51,7 @@ var Pokemon = React.createClass({
     return (
         <div className="col-sm-6 col-md-4">
           <div className="thumbnail">
-            <img src="#" />
+            <PokemonImage id={this.state.id} />
             <div className="caption">
               <h3>{this.state.name}</h3>
               <div className="pokemon-types">
@@ -56,7 +65,6 @@ var Pokemon = React.createClass({
             </div>
           </div>
         </div>
-
     );
   }
 });
@@ -103,6 +111,7 @@ var Pokedex = React.createClass({
           <form onSubmit={this.handleSubmit}>
             <input onChange={this.onChange} value={this.state.text} />
           </form>
+          <br/>
         </div>
         <div className="pokedex-container">
           <PokedexList source="http://pokeapi.co/api/v1/pokedex/1/" />
